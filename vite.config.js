a2baf3c5b40fef787f82a6947aaf53937d6b53d0
blackingg <odetundemubarak04@gmail.com>
2026-03-10 14:31:01 +0100

    feat: Implemented manualChunks to split the vendor libraries and utility functions into separate files.

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,35 @@
-import glsl from 'vite-plugin-glsl'
-import { defineConfig } from 'vite'
-import path from 'path'
+import glsl from "vite-plugin-glsl";
+import { defineConfig } from "vite";
+import path from "path";
 
-const dirname = path.resolve()
+const dirname = path.resolve();
 
 export default defineConfig({
-    resolve:
-    {
-        alias:
-        {
-            '@' : path.resolve(dirname, './sources/Game')
-        }
+  resolve: {
+    alias: {
+      "@": path.resolve(dirname, "./sources/Game"),
     },
-    plugins:
-    [
-        glsl({ watch: true })
-    ],
-    server:
-    {
-        host: true,
-        open: true
-    }
-})
+  },
+  plugins: [glsl({ watch: true })],
+  server: {
+    host: true,
+    open: true,
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["three"],
+          utils: [
+            "gl-matrix",
+            "lil-gui",
+            "simplex-noise",
+            "seedrandom",
+            "stats.js",
+            "events",
+          ],
+        },
+      },
+    },
+  },
+});
